10ddacef8c46d93a2318c328409a68bcbf7ee707
will-s-h <107830908+will-s-h@users.noreply.github.com>
2022-08-17 14:05:27 -0700

    Update dfs.cpp

--- graphs/dfs.cpp
+++ graphs/dfs.cpp
@@ -10,22 +10,29 @@ vector<int> edges[maxN] = {};
 int group[maxN] = {};
 
 void dfs(int n, int g){
-  group[n] = g;
-  for(int next : edges[n]){
-    if(group[next] == 0) dfs(next);
-  }
+	group[n] = g;
+	for(int next : edges[n]){
+		if(group[next] == 0) dfs(next, g);
+	}
 }
 
 int main(){
-  cin >> N >> M; //input number of nodes, number of edges
-  for(int i = 0; i < M; i++){ //set edges, 0-indexed
-    int a, b; cin >> a >> b; a--; b--;
-    edges[a].push_back(b);
-    edges[b].push_back(a);
-  }
-  
-  int group = 1;
-  for(int i = 0; i < N; i++){
-    if(group[i] == 0) dfs(i, group);
-  }
+	cin >> N >> M; //input number of nodes, number of edges
+	for(int i = 0; i < M; i++){ //set edges, 0-indexed
+		int a, b; cin >> a >> b; a--; b--;
+		edges[a].push_back(b);
+		edges[b].push_back(a);
+	}
+
+	int g = 1;
+	for(int i = 0; i < N; i++){
+		if(group[i] == 0) {
+			dfs(i, g);
+			g++;
+		}
+	}
+
+	for(int i = 0; i < N; i++){
+		cout << group[i] << " ";
+	}
 }
