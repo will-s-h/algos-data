502d0772d32175c3ff915804e7362a73b5bbb6be
will-s-h <107830908+will-s-h@users.noreply.github.com>
2022-08-17 14:01:26 -0700

    Create dfs.cpp

---
+++ graphs/dfs.cpp
@@ -0,0 +1,31 @@
+// Depth First Search (DFS) using recursion; O(N+M)
+// basic example, labelling each node by a number which indicates which connected component they are a part of
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int maxN = 1e5;
+int N, M;
+vector<int> edges[maxN] = {};
+int group[maxN] = {};
+
+void dfs(int n, int g){
+  group[n] = g;
+  for(int next : edges[n]){
+    if(group[next] == 0) dfs(next);
+  }
+}
+
+int main(){
+  cin >> N >> M; //input number of nodes, number of edges
+  for(int i = 0; i < M; i++){ //set edges, 0-indexed
+    int a, b; cin >> a >> b; a--; b--;
+    edges[a].push_back(b);
+    edges[b].push_back(a);
+  }
+  
+  int group = 1;
+  for(int i = 0; i < N; i++){
+    if(group[i] == 0) dfs(i, group);
+  }
+}
