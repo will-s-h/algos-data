8cf199de34f943af4b936470e3e8025cdce28ec1
will-s-h <107830908+will-s-h@users.noreply.github.com>
2022-08-17 14:19:19 -0700

    Create floodfill.cpp

---
+++ graphs/floodfill.cpp
@@ -0,0 +1,43 @@
+//example of a floodfill that counts size of each connected component
+//O(N*M)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MN = 1000;
+
+int grid[MN][MN];  // the grid itself
+int row_num;
+int col_num;
+bool visited[MN][MN];  // keeps track of which nodes have been visited
+int curr_size = 0;  // reset to 0 each time we start a new component
+
+void floodfill(int r, int c, int color){
+	if (
+		(r < 0 || r >= row_num || c < 0 || c >= col_num)  // if out of bounds
+		|| grid[r][c] != color  // wrong color
+		|| visited[r][c]  // already visited this square
+	) return;
+
+	visited[r][c] = true; // mark current square as visited
+	curr_size++; // increment the size for each square we visit
+
+	// recursively call flood fill for neighboring squares
+	floodfill(r, c + 1, color);
+	floodfill(r, c - 1, color);
+	floodfill(r - 1, c, color);
+	floodfill(r + 1, c, color);
+}
+
+int main() {
+	for (int i = 0; i < row_num; i++) {
+		for (int j = 0; j < col_num; j++) {
+			if (!visited[i][j]) {
+				curr_size = 0;
+				floodfill(i, j, grid[i][j]);
+        cout << cur_size << " ";
+			}
+		}
+	}
+	return 0;
+}
